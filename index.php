814eaaddc10d71840c25daee15ac880062e3db14
Vlasnn <vlas73@gmail.com>
2024-08-12 03:25:22 -0400

    Setup Boostrap files, custom styles file and icons link

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MEDICINAS | Login</title>
+  <!-- Bootstrap Style -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Custom Style -->
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- Icons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+
+
+
+
+  <!-- Scripts -->
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
