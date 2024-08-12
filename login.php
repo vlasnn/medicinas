e94ff1fdd2bcd25fd9ef48f5289653c6e547681c
Vlasnn <vlas73@gmail.com>
2024-08-12 03:52:28 -0400

    setup login page

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MEDICINAS | Login</title>
+  <!-- Bootstrap Style -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Custom Style -->
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- Icons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+  <main>
+    <section class="login">
+      <form action="" autocomplete="off" method="POST">
+
+        <div class="mt-3 text-center">
+          <h1>CONTROL MEDICINAS</h1>
+          <H2>LOGIN</H2>
+        </div>
+
+        <div class="mt-3">
+          <label for="username" class="label-form">Username:</label>
+          <input type="text" class="form-control" name="username" autofocus>
+        </div>
+
+        <div class="mt-3">
+          <label for="password" class="label-form">Password:</label>
+          <input type="password" class="form-control" name="password" autofocus>
+        </div>
+
+        <div class="mt-3">
+          <input type="submit" class="form-control" name="Login" value="login">
+        </div>
+
+
+      </form>
+    </section>
+
+  </main>
+
+
+
+
+
+  <!-- Scripts -->
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
